d84f4a0fb09394b62e0d4f27ef607630f1ff6369
tcdw <admin@tcdw.net>
2017-11-19 22:53:06 +0800

    菜单调整

--- static/js/blog.js
+++ static/js/blog.js
@@ -14,19 +14,35 @@ timeSince = function (date) {
         if (gap >= ((i <= 0) ? 0 : gapType[i - 1].max) && gap < gapType[i].max) {
             return Math.floor(gap / ((i <= 0) ? 1 : gapType[i - 1].max)) + gapType[i].suffix;
         } else if (gap > gapType[gapType.length - 1].max || gap < 0 || i >= gapType.length) {
-            var ds = new Date(date).toDateString().split(" ");
-            return ds[1] + " " + ds[2] + ", " + ds[3];
+            var ds = new Date(date);
+            return ds.getFullYear() + " 年 " + ds.getMonth() + " 月 " + ds.getDay() + " 日";
         } else {
             i += 1;
         }
     }
 }
 
+var uri = new URL(location.href);
+var nav_div = document.getElementById("head-menu");
+var links = nav_div.getElementsByTagName("a");
+var index = -1;
+var url_path = uri.pathname;
+if (url_path !== "") {
+    for (let i = links.length; i--;) {
+        if (links[i].href.indexOf(url_path) !== -1 && url_path !== "/") {
+            index = i;
+            break;
+        }
+    }
+}
+
+links[index].parentNode.className = 'nav-list-link active';
+
 var dateItem = document.getElementsByTagName("time");
-for (var i in dateItem) {
+for (let i in dateItem) {
     if (dateItem[i].innerHTML != "") {
         let enTime = new Date(dateItem[i].innerHTML);
-        dateItem[i].title = enTime.toUTCString();
+        dateItem[i].title = enTime.getFullYear() + " 年 " + enTime.getMonth() + " 月 " + enTime.getDay() + " 日 " + enTime.getHours().toFixed(2) + ":" + enTime.getMinutes().toFixed(2);
         dateItem[i].innerHTML = timeSince(enTime.getTime());
     }
 }
