c864164f64cf25af4ead42ab77566f3598e83473
jianghua huang <qwe7002@hotmail.com>
2017-11-23 16:56:34 +0800

    修复安装脚本

--- install.sh
+++ install.sh
@@ -4,9 +4,9 @@ if [ $(basename `pwd`) != "templates" ];then
     echo "[Error] Please do this in the templates directory!"
     exit
 fi
-if [ ! -d "apollo" ]; then
-    git clone https://github.com/SilverBlogTheme/kico.git
-    cd apollo
+if [ ! -d "kico" ]; then
+    git clone https://github.com/SilverBlogTheme/kico.git --depth 1
+    cd kico
     ln -s $(pwd)/static ../static/kico
     echo "Remember, if you want to make local changes, edit files in /templates/kico/source folder, then use gulp to build static files!"
 fi
